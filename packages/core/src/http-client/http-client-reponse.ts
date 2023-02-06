a60cbbc8be5f96b18f827ec09fba73c26815e55a
stLmpp <gui.stlmpp@hotmail.com>
2023-02-06 20:45:25 -0300

    chore(): override clone method on HttpClientResponse

--- packages/core/src/http-client/http-client-reponse.ts
+++ packages/core/src/http-client/http-client-reponse.ts
@@ -2,4 +2,8 @@ export class HttpClientResponse<T> extends Response {
   override json(): Promise<T> {
     return super.json();
   }
+
+  override clone(): HttpClientResponse<T> {
+    return super.clone();
+  }
 }
